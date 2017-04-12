64253c47990542c0ac644112d3b70a77770f8067
husigntospeech <husigntospeech@gmail.com>
2017-04-12 13:28:16 -0400

    Cleaned up opencv_handler code

--- SignLanguageToSpeechServer/backend/open_cv_handler.py
+++ SignLanguageToSpeechServer/backend/open_cv_handler.py
@@ -9,81 +9,102 @@ import pyttsx
 class OpenCVHandler(object):
 
 	# target rect coordinates (250,275),(50,0)
-	def get_text_translation_from_image(self, image_name):
-
+	def get_text_translation_from_image(self, image_name, image_id):
 		(version, _, _) = cv2.__version__.split('.')
-		value = (35, 35)
-		corrDict = {}
 
 		# read captured image
-		img = cv2.imread(image_name)
-		height, width, channels = img.shape
-		print "%s %s" % (width, height)
-		cv2.rectangle(img,(width / 2, height / 3),(width / 5,0),(0,255,0),0)
+		cropped_image = read_captured_image_and_crop(image_name)
 
-		crop_img = img[0:(height / 3), (width / 5):(width / 2)]
-		cv2.imshow('im', crop_img)
-		cv2.imwrite('cropped_image.jpg',crop_img)
+		cv2.imshow('im', cropped_image)
 
-		grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-		blurred = cv2.GaussianBlur(grey, value, 0)
-		_, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+		# Write cropped image for debugging purposes.
+		cropped_image_name = 'cropped_image_%s.jpg' % (image_id)
+		cv2.imwrite(cropped_image_name, cropped_image)
 
-		# find countours in caputred image
-		if version is '3':
-		    image, contours, hierarchy = cv2.findContours(thresh1.copy(), \
-		           cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		grey_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
+		contours = self.get_contours_of_image(grey_image)
 
-		elif version is '2':
-		    contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, \
-		           cv2.CHAIN_APPROX_NONE)
+	    # Gets largest contour.
+		image_largest_contour = self.get_largest_contour(contours)
 
+		corrDict = {}
+		stock_image_paths = self.get_paths_to_stored_images()
 
-	  	cnt = max(contours, key = lambda x: cv2.contourArea(x))
-	  	print(ord('A'), " ", ord('G'))
+		# Iterate through each image path
+		# Get each image contour
+		# Compare to the received image's contour
+		# Put that correlation into dictionary
+		for stock_image_path in stock_image_paths:
+			# Get letter
+			pieces = stock_image_path.split('/')
+			letter = pieces[0].split('_')[1]
+			stock_image = cv2.imread(stock_image_path)
 
-		for x in range(ord('A'), ord('G')):
-			count = 1
-			corr = 5
-        	print(chr(x))
+			contours = self.get_contours_of_image(stock_image)
+			stock_largest_contour = self.get_largest_contour(contours)
 
-        	while not cv2.imread('sign_' + chr(x) +'/sign_' + chr(x) + str(count) + '.jpg', 0) is None :
-        		grey = cv2.imread('sign_' + chr(x) +'/sign_' + chr(x) + str(count) + '.jpg', 0)
-        		blurred = cv2.GaussianBlur(grey, value, 0)
-        		_, letter_thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+			corr = self.compare_shapes(stock_largest_contour, largest_contour)
+			corrDict[corr] = chr(letter)
 
-        		if version is '3':
-        			image, letter_contours, hierarchy = cv2.findContours(letter_thresh.copy(), \
-	                   cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+		x, y, w, h = cv2.boundingRect(image_largest_contour)
 
-        		elif version is '2':
-        			letter_countours, hierarchy = cv2.findContours(letter_thresh.copy(),cv2.RETR_TREE, \
-	                   cv2.CHAIN_APPROX_NONE)
-
-                letter_cnt = max(letter_contours, key = lambda x: cv2.contourArea(x))
-
-                if cv2.matchShapes(cnt,letter_cnt,1,0.0) < corr:
-	            	corr = cv2.matchShapes(cnt,letter_cnt,1,0.0)
-            	count +=1
-
-	        count -= 1
-	        corrDict[corr] = chr(x) 
-
-
-		x,y,w,h = cv2.boundingRect(cnt)
-
-		sketch = np.zeros(crop_img.shape, np.uint8)
-		cv2.drawContours(sketch,[cnt], 0, (0,255,0),0)
-		cv2.imshow("contour",sketch)
-
+		sketch = np.zeros(cropped_image.shape, np.uint8)
+		cv2.drawContours(sketch,[image_largest_contour], 0, (0, 255, 0), 0)
+		cv2.imshow("Contour", sketch)
 
 		for key in corrDict:
 			print '%s : %s' % (corrDict[key], key)
 
 		corr_list = corrDict.keys()
 		corr_list.sort()
-		return corrDict[corr_list[0 ]]
 
+		return corrDict[corr_list[0]]
+
+	def compare_shapes(shape1, shape2):
+		return cv2.matchShapes(shape1, shape2, 1, 0.0)
+
+	def get_largest_contour(contours):
+		contours = self.get_contours_of_image(stock_image)
+		largest_contour = max(contours, key = lambda x: cv2.contourArea(x))
+
+		return largest_contour
+
+	def get_contours_of_image(self, image):
+		value = (35, 35)
+		blurred = cv2.GaussianBlur(image, value, 0)
+		_, letter_thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+		if version is '3':
+			image, letter_contours, hierarchy = cv2.findContours(letter_thresh.copy(), \
+			   cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+		elif version is '2':
+			letter_countours, hierarchy = cv2.findContours(letter_thresh.copy(),cv2.RETR_TREE, \
+			   cv2.CHAIN_APPROX_NONE)
+
+		return contours
+
+
+	def get_paths_to_stored_images(self):
+		paths = []
+		for letter_value in range(ord('A'), ord('G')):
+	        folder_path = 'sign_%s' % (chr(letter_value))
+	        directory = os.listdir(folder_path)
+	        for image_name in directory:
+	            valid_image_name = image_name.lower().endswith('.jpg')
+	            if valid_image_name:
+	                file_path = '%s/%s' % (folder_path, image_name)
+	                paths.append(file_path)
+
+		return paths
+
+	def read_captured_image_and_crop(self, image_name):
+		img = cv2.imread(image_name)
+		height, width, channels = img.shape
+		cv2.rectangle(img,(width / 2, height / 3),(width / 5,0),(0,255,0),0)
+		cropped_image = img[0:(height / 3), (width / 5):(width / 2)]
+
+		return cropped_image
 
 	def play_audio_translation_from_text(self, text):
 		eng = pyttsx.init()
--- SignLanguageToSpeechServer/backend/server.py
+++ SignLanguageToSpeechServer/backend/server.py
@@ -1,7 +1,10 @@
 import base64
 import logging
+import os
 
-from open_cv_handler import OpenCVHandler
+from uuid import uuid4
+
+# from open_cv_handler import OpenCVHandler
 from websocket_server import WebsocketServer
 
 def on_client_connect(client, server):
@@ -9,12 +12,20 @@ def on_client_connect(client, server):
 
 def on_client_message(client, server, message):
     print 'Got image string.'
-    # Processing image data
-    image_name = 'image.jpg'
+
+    # Process image data.
     decoded_str = base64.decodestring(message)
+
+    # Give each image that comes in a unique universal id because each the
+    # processes each client in a SEPARATE thread.
+    image_id = uuid.uuid4().hex
+    image_name = 'image_%s' % (image_id)
+
+    # Write image to server.
     write_image_to_system(decoded_str, image_name)
 
     print 'Getting text translation.'
+
     opencv_handler = OpenCVHandler()
     # Get translation from OpenCV then play text audio
     text_trans = opencv_handler.get_text_translation_from_image(image_name)
